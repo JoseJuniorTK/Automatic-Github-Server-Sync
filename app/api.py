f9b6b59a9200155d88ffced3b7bc261860c26538
JoseJuniorTK <69799917+JoseJuniorTK@users.noreply.github.com>
2022-10-18 11:13:04 -0300

    Add files via upload

---
+++ app/api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, File, UploadFile, Response, Request, Body
+from fastapi.middleware.cors import CORSMiddleware
+import subprocess
+
+
+app = FastAPI()
+
+origins = [
+    "https://github.com/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/x1243")
+async def main(payload: dict = Body(...)):
+    update = f'tmux send-keys -t GitAcess "git pull" ENTER'
+    subprocess.call([update], shell=True)
+    return 0
